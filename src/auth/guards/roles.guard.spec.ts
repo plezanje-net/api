a4562909c5f2fef9f9623f7a5b3cb967b408b62e
MaticDiba <mczaar@gmail.com>
2020-09-07 22:58:35 +0200

    all tests pass

--- src/auth/guards/roles.guard.spec.ts
+++ src/auth/guards/roles.guard.spec.ts
@@ -1,7 +1,23 @@
 import { RolesGuard } from './roles.guard';
+import { MockType, serviceMockFactory } from '../../../test/unit/helpers';
+import { Reflector } from '@nestjs/core';
+import { Test, TestingModule } from '@nestjs/testing';
 
 describe('RolesGuard', () => {
+  let guard: RolesGuard;
+  let reflectorMock: MockType<Reflector>
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        RolesGuard,
+        { provide: Reflector, useFactory: serviceMockFactory },
+      ],
+    }).compile();
+
+    guard = module.get<RolesGuard>(RolesGuard);
+    reflectorMock = module.get(Reflector)
+  });
   it('should be defined', () => {
-    expect(new RolesGuard()).toBeDefined();
+    expect(guard).toBeDefined();
   });
 });
