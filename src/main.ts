33c4124fd6eb3d2f0f82aeefd1d91249569b45c0
Anze Demsar <demshy@gmail.com>
2020-11-17 14:11:04 +0100

    add cors to nest app

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
   const PORT = Number(process.env.PORT) || 3000;
   await app.listen(PORT);
