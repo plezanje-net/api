bec8085aa1d659d67a4c920a9726fe3864dec2a0
Anze Demsar <demshy@gmail.com>
2021-05-03 15:51:45 +0200

    add activity type to graphql fields

--- src/activities/entities/activity.entity.ts
+++ src/activities/entities/activity.entity.ts
@@ -1,65 +1,79 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, BaseEntity, ManyToOne, OneToMany } from "typeorm";
-import { ObjectType, Field, Int } from "@nestjs/graphql";
-import { Crag } from "src/crags/entities/crag.entity";
-import { ActivityRoute } from "./activity-route.entity";
-import { User } from "src/users/entities/user.entity";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  BaseEntity,
+  ManyToOne,
+  OneToMany,
+} from 'typeorm';
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Crag } from 'src/crags/entities/crag.entity';
+import { ActivityRoute } from './activity-route.entity';
+import { User } from 'src/users/entities/user.entity';
 
 export enum ActivityType {
-    CRAG = "crag",
-    CLIMBING_GYM = "climbingGym",
-    TRAINING_GYM = "trainingGym",
-    PEAK = "peak",
-    ICE_FALL = "iceFall",
-    OTHER = "other"
+  CRAG = 'crag',
+  CLIMBING_GYM = 'climbingGym',
+  TRAINING_GYM = 'trainingGym',
+  PEAK = 'peak',
+  ICE_FALL = 'iceFall',
+  OTHER = 'other',
 }
 
 @Entity()
 @ObjectType()
 export class Activity extends BaseEntity {
-    @PrimaryGeneratedColumn("uuid")
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @ManyToOne(() => Crag, { nullable: true })
-    crag: Promise<Crag>;
+  @ManyToOne(() => Crag, { nullable: true })
+  crag: Promise<Crag>;
 
-    @Column({
-        type: "enum",
-        enum: ActivityType,
-        default: ActivityType.OTHER
-    })
-    type: ActivityType;
+  @Column({
+    type: 'enum',
+    enum: ActivityType,
+    default: ActivityType.OTHER,
+  })
+  @Field()
+  type: ActivityType;
 
-    @Column({ nullable: true })
-    @Field()
-    name: string;
+  @Column({ nullable: true })
+  @Field()
+  name: string;
 
-    @Column({ nullable: true })
-    @Field()
-    date: Date;
+  @Column({ nullable: true })
+  @Field()
+  date: Date;
 
-    @Column({ nullable: true })
-    duration: number;
+  @Column({ nullable: true })
+  duration: number;
 
-    @Column({ nullable: true })
-    notes: string;
+  @Column({ nullable: true })
+  notes: string;
 
-    @Column({ nullable: true })
-    partners: string;
+  @Column({ nullable: true })
+  partners: string;
 
-    @CreateDateColumn()
-    created: Date;
+  @CreateDateColumn()
+  created: Date;
 
-    @UpdateDateColumn()
-    updated: Date;
+  @UpdateDateColumn()
+  updated: Date;
 
-    @Column({ nullable: true })
-    legacy: string;
+  @Column({ nullable: true })
+  legacy: string;
 
-    @OneToMany(() => ActivityRoute, route => route.activity, { nullable: true })
-    @Field(() => [ActivityRoute])
-    routes: Promise<ActivityRoute[]>;
+  @OneToMany(
+    () => ActivityRoute,
+    route => route.activity,
+    { nullable: true },
+  )
+  @Field(() => [ActivityRoute])
+  routes: Promise<ActivityRoute[]>;
 
-    @ManyToOne(() => User, {nullable: false})
-    @Field(() => User)
-    user: Promise<User>;
+  @ManyToOne(() => User, { nullable: false })
+  @Field(() => User)
+  user: Promise<User>;
 }
